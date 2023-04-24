e7bd2373df7a2b7956f51ecd6b76eb4cb777213e
Divyesh Ved <82323146+DivyeshVed@users.noreply.github.com>
2023-04-24 13:34:25 -0400

    Creating confusion matrix
    Creating the confusion matrix for the logistic regression model. Adding to the model files to save results as pickle files and then read them while creating confusion matrices.

---
+++ confusion_matrix.py
@@ -0,0 +1,10 @@
+# THIS FILE IS GOING TO CREATE THE CONFUSION MATRIX FOR VARIOUS MODELS.
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+# We are going to create the confusion matrices for each model. Thus we are going to have 4 overall confusion matrices. 
+# The way we are going to do this is we are going to make the confusion matrix for each model, for each year, for ewach conference, and then add them all together up. 
+# This will give us the overall confusion matrix for that model. 
+
+
+
